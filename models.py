318d60fccc3b3ae28e802b5f619e6684ed6a7452
Ysabella Romero <ysabellaromero5@gmail.com>
2021-07-03 19:56:40 +0100

    Update

--- models.py
+++ models.py
@@ -9,10 +9,10 @@ class Net(nn.Module):
     def __init__(self, pretrained, num_outputs):
         super().__init__()
         self.n_labels = num_outputs
-        model = models.densenet121(pretrained=True)                                     #Instanciar um modelo pré-treinado baixará seus pesos para um diretório de cache
+        model = models.vgg16(pretrained=True)                                           #Instanciar um modelo pré-treinado baixará seus pesos para um diretório de cache
         model = nn.Sequential(*list(model.children())[:-1])                             #Novo modelo criado através da lista com todas as camadas (à exceção da última) do modelo pré-treinado                            
         last_dimension = torch.flatten(model(torch.randn(1, 3, 224, 224))).shape[0]     #Dimensão da última camada do modelo     
-        self.classifier = nn.Sequential(     
+        self.classifier = nn.Sequential(                                                #torch.randn(1, 3, 224, 224) --> tensor com dimensão 1x3x224x224 
             model, 
             nn.Flatten(),                                                               #Aplana o tensor 
             nn.Dropout(0.2),                                                            #Dropout é uma técnica que seleciona 'neurals' aleatoriamente e os ignora
@@ -36,3 +36,5 @@ class Net(nn.Module):
         
     
 n_labels = 4
+
+Net(True, n_labels)
