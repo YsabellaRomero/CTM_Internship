a69fd74a7108adc2d9675608cfcd84e502c6c303
Ysabella Romero <ysabellaromero5@gmail.com>
2021-07-05 21:21:06 +0100

    done

--- models.py
+++ models.py
@@ -11,8 +11,8 @@ class Net(nn.Module):
         self.n_labels = num_outputs
         model = models.vgg16(pretrained=True)                                           #Instanciar um modelo pré-treinado baixará seus pesos para um diretório de cache
         model = nn.Sequential(*list(model.children())[:-1])                             #Novo modelo criado através da lista com todas as camadas (à exceção da última) do modelo pré-treinado                            
-        last_dimension = torch.flatten(model(torch.randn(1, 3, 224, 224))).shape[0]     #Dimensão da última camada do modelo     
-        self.model = nn.Sequential(                                                #torch.randn(1, 3, 224, 224) --> tensor com dimensão 1x3x224x224 
+        last_dimension = torch.flatten(model(torch.randn(1, 3, 224, 224))).shape[0]      #Dimensão da última camada do modelo     
+        self.model = nn.Sequential(                                                     #torch.randn(1, 3, 224, 224) --> tensor com dimensão 1x3x224x224 
             model, 
             nn.Flatten(),                                                               #Aplana o tensor 
             nn.Dropout(0.2),                                                            #Dropout é uma técnica que seleciona 'neurals' aleatoriamente e os ignora
@@ -35,6 +35,5 @@ class Net(nn.Module):
         return F.softmax(outputs, dim=1)
         
     
-#n_labels = 4
-
-#Net(True, n_labels)
+n_labels = 4
+Net(True, n_labels)
