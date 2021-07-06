ed5e2733aeedc4b1c98de0ba3b247d3daf796a6f
Ysabella Romero <ysabellaromero5@gmail.com>
2021-07-06 18:19:47 +0100

    update

--- models.py
+++ models.py
@@ -6,12 +6,12 @@ from torchvision import models
 criterion = nn.CrossEntropyLoss()
 
 class Net(nn.Module):
-    def __init__(self, pretrained, num_outputs):
+    def __init__(self, pretrained_model):
         super().__init__()
-        self.n_labels = num_outputs
+        #model = getattr(models, pretrained_model)(pretrained=True)
         model = models.vgg16(pretrained=True)                                           #Instanciar um modelo pré-treinado baixará seus pesos para um diretório de cache
         model = nn.Sequential(*list(model.children())[:-1])                             #Novo modelo criado através da lista com todas as camadas (à exceção da última) do modelo pré-treinado                            
-        last_dimension = torch.flatten(model(torch.randn(1, 3, 224, 224))).shape[0]      #Dimensão da última camada do modelo     
+        last_dimension = torch.flatten(model(torch.randn(1, 3, 224, 224))).shape[0]     #Dimensão da última camada do modelo     
         self.model = nn.Sequential(                                                     #torch.randn(1, 3, 224, 224) --> tensor com dimensão 1x3x224x224 
             model, 
             nn.Flatten(),                                                               #Aplana o tensor 
@@ -22,21 +22,21 @@ class Net(nn.Module):
             nn.Linear(512,256),
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.Linear(256, num_outputs)
+            nn.Linear(256, 4)
         )
 
-    def foward(self, x):
+    def forward(self, x):
         return self.model(x)
     
-    def loss(self, outputs, labels):
-        return criterion(outputs, labels)
+    def loss(self, pred, labels):
+        return criterion(pred, labels)
 
     def softmax(self, outputs):
         return F.softmax(outputs, dim=1)
-        
+    
     def pred(self, pred):
         return torch.max(pred,1)
         
     
-n_labels = 4
-Net(True, n_labels)
+#n_labels = 4
+#Net(True, n_labels)
