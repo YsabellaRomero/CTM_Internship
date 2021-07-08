7c617a5e2aa92d5ac31f57f97dac99caaa41bd66
Ysabella Romero <ysabellaromero5@gmail.com>
2021-07-08 22:33:10 +0100

    update

--- metrics.py
+++ metrics.py
@@ -1,12 +1,13 @@
 from sklearn import metrics
 import torch.nn as nn
-import argparse 
 import numpy as np
 import torch
-import os
+import pickle
 import train
 
 path_init = train.path_init
+
+criterion = nn.CrossEntropyLoss()
 
 class metricas(nn.Module):
 
@@ -30,28 +31,24 @@ class metricas(nn.Module):
     return criterion(yhat, y)
 
 
-YY = [pickle.load(open(train.path_init, 'rb'))[fold]['test'][1] for fold in range(5)]
+YY = [pickle.load(open(path_init, 'rb'))[fold]['test'][1] for fold in range(5)]
 
-def evaluate(YY, outputs, architecture=args.architecture):
-  resultados = []
+def evaluate(YY, outputs, architecture = train.args.architecture):
+    
+    resultados = []
 
     for fold in range(5):
         Y = YY[fold]
-        start = f'output-architecture-{architecture}-fold-{fold}-batchsize-{args.batchsize}-dropout-{args.dropout}-lr-{args.lr}'
+        start = f'output-architecture-{architecture}-fold-{fold}-batchsize-{train.args.batchsize}-dropout-{train.args.dropout}-lr-{train.args.lr}'
         filename = [o for o in outputs if o.split('/')[-1].startswith(start)]
         assert len(filename), f'Empty filenames starting with {start}'
         Yhat = np.loadtxt(filename[0], delimiter=',')
 
         resultados.append(metricas.auc(Y, Yhat))
         resultados.append(metricas.mae(Y, Yhat))
-        resultados.append(metricas.acc(Y, Yhat))
-        resultados.append(metricas.prec(Y, Yhat))
+        resultados.append(metricas.accuracy(Y, Yhat))
+        resultados.append(metricas.precision(Y, Yhat))
         resultados.append(metricas.recall(Y, Yhat))
         resultados.append(metricas.loss(Yhat, Y))
 
     return resultados
-
-
-
-
-
